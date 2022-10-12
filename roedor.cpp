bca10c8bd4142ff063d298a9e2efd66503647ab9
Guido Menendez <guidomenendez0@gmail.com>
2022-10-12 14:52:12 -0300

    -Se corrige el error de codigo que impedia crear clases de tipo Nodo. -Se agrega el archivo "roedor.cpp" que no fue incluido por error en el último commit

---
+++ roedor.cpp
@@ -0,0 +1,39 @@
+#include "roedor.h"
+#include <iostream>
+
+const int AUMENTO_HAMBRE_BASE = 10;
+const int HAMBRIENTO = 100;
+const int BONUS_DORMILON = 2;
+const int PENALIZACION_JUGUETON = 2;
+const int SATISFECHO = 0;
+
+Roedor::Roedor(std::string nombre, int edad, std::string tamanio, char especie, std::string personalidad) : Animal(nombre, edad, tamanio, especie, personalidad){
+    this->tipo_de_comida = "atun";
+}
+
+void Roedor::comer(){
+    if(hambre_actual != SATISFECHO){
+        hambre_actual = SATISFECHO;
+        std::cout << this->nombre << " comio " << this->tipo_de_comida << "." << std::endl;
+    } else {
+        std::cout << this->nombre << " no tiene hambre." << std::endl;
+    }
+}
+
+void Roedor::pasar_el_tiempo(){
+    aumentar_hambre();
+}
+
+void Roedor::aumentar_hambre(){
+    if(this->personalidad == "dormilon" && !esta_hambriento()){
+        hambre_actual = hambre_actual + (AUMENTO_HAMBRE_BASE / BONUS_DORMILON);
+    } else if(this->personalidad == "jugueton" && !esta_hambriento()) {
+        hambre_actual = hambre_actual + (AUMENTO_HAMBRE_BASE * PENALIZACION_JUGUETON);
+    } else if (!esta_hambriento()) {
+        hambre_actual = hambre_actual + AUMENTO_HAMBRE_BASE;
+    }
+}
+
+bool Roedor::esta_hambriento(){
+    return (hambre_actual == HAMBRIENTO);
+}
