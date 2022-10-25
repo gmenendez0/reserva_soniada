16b4a619d8e2bbc4e4d21c525ea56ecbb3b2d961
MartinPedutto <115514162+MartinPedutto@users.noreply.github.com>
2022-10-25 03:20:51 -0300

    Update menu_controller.cpp

--- menu_controller.cpp
+++ menu_controller.cpp
@@ -1,6 +1,7 @@
 #include "menu_controllers.h"
 #include "cuidar_animales_controllers.h"
 #include "listar_animales.h"
+#include "rescatar_animal.h"
 #include "adoptar_animal_controllers.h"
 
 #include "guardar.h"
@@ -20,13 +21,13 @@ const int GUARDAR_Y_SALIR = 6;
 const int UNA_ACCION = 1;
 
 //Pre Debe recibir una respuesta
-//Post Devuelve true en caso de que la respuesta sea válida (valores 1 - 6 inclusive), false caso contrario
+//Post Devuelve true en caso de que la respuesta sea vÃ¡lida (valores 1 - 6 inclusive), false caso contrario
 bool respuesta_es_valida(int respuesta){
     return (respuesta >= LISTAR_ANIMALES && respuesta <= GUARDAR_Y_SALIR);
 }
 
 //Pre Debe recibir una respuesta
-//Post Chequea si la respuesta es válida, en caso de no serla vuelve a pedir una respuesta al usuario
+//Post Chequea si la respuesta es vÃ¡lida, en caso de no serla vuelve a pedir una respuesta al usuario
 void chequear_entrada(int &respuesta){
     while(!respuesta_es_valida(respuesta)){
         cout << "Usted a ingresado un valor invalido. Porfavor, vuelva a intentar:  ";
@@ -84,7 +85,7 @@ void activar_opcion_elegida(int opcion_elegida, Lista<Animal*> &registro_de_anim
             break;
 
         case RESCATAR_ANIMAL:
-            //funcion de rescatar animal
+            rescatar_animal(registro_de_animales);
             break;
 
         case BUSCAR_ANIMAL:
